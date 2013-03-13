06b4845e34d21a5ed12eb75fffe9e19a18b643bd
CubeSugar <charleywei1110@gmail.com>
2013-03-13 19:03:46 +0800

    完善注释

--- clist.c
+++ clist.c
@@ -14,6 +14,7 @@
 void initCList(Clist *clist, void (*destroy)(void *data))
 {
 	clist.size    = 0;
+	//未指定默认释放方法
 	clist.destroy = destroy;
 	clist.head    = NULL;
 }
@@ -33,7 +34,6 @@ void destroyCList(CList *clist)
  	{
  		if (removeCListElmt(clist, clist.head, (void **)&data) == 0 && clist.destroy != NULL)
  		{
- 			/* code */
  			clist.destroy(data);
  		}
  	}
@@ -53,19 +53,20 @@ int insertCListElmt(CList *clist, CListElmt *celement, const void *data)
  	CListElmt *new_element;
  	if ((new_element = (CListElmt *)malloc(sizeof(CListElmt))) == NULL)
  	{
- 		/* code */
  		return -1;
  	}
 
  	new_element.data = (void *)data;
+
  	if (clist.size == 0)
  	{
- 		/* code */
+ 		//循环链表为空时
  		new_element.next = new_element;
  		clist.head = new_element;
  	}
  	else
  	{
+ 		//非空时
  		new_element.next = celement.next;
  		celement.next = new_element;
  	}
@@ -79,18 +80,39 @@ int insertCListElmt(CList *clist, CListElmt *celement, const void *data)
  *
  *return:	success 0, false -1
  *
- *function:	删除指定元素
+ *function:	删除指定元素的后继
  */
 int removeCListElmtNext(CList *clist, CListElmt *celement, void **data)
 {
 	CListElmt *old_celement;
 
+	//防止在空链表中执行方法
 	if (clist.size == 0)
 	{
-		/* code */
 		return -1;
 	}
 
-	*data = celement.
+	*data = celement.next.data;
 
+	if (celement.next = celement)
+	{
+		//循环链表只有一个元素
+		old_celement = celement.next;
+		clist.head   = NULL;
+	}
+	else
+	{
+		old_celement  = celement.next;
+		celement.next = celement.next.next;
+		if (old_celement == clist.head)
+		{
+			//若celement为链表尾节点，则待删除节点为头节点
+			clist.head = old_celement.next;
+		}
+	}
+
+	free(old_celement);
+
+	clist.size--;
+	return 0;
 }
