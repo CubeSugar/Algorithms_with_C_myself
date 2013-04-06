2eb58cb1c4305475c0b4b228952854a8d1ca66e5
CubeSugar <charleywei1110@gmail.com>
2013-04-06 18:02:03 +0800

    二叉搜索树

--- tree_binary.h
+++ tree_binary.h
@@ -10,6 +10,7 @@ typedef struct BiTreeNode_
 	struct BiTreeNode_ *right;
 } BiTreeNode;
 
+//binary trees
 typedef struct BiTree_
 {
 	int size;
@@ -19,60 +20,68 @@ typedef struct BiTree_
 } BiTree;
 
 //public interface
-/*name:		initBiTree()
+/*---------------------------------------------------------
+ *name:		initBiTree()
  *input:	BiTree *tree, void (*destroy)(void *data)
  *return:	none
  *function:	initiate BiTree
- */
+ *---------------------------------------------------------*/
 void initBiTree(BiTree *tree, void (*destroy)(void *data));
 
-/*name:		destroyTree()
+/*---------------------------------------------------------
+ *name:		destroyTree()
  *input:	BiTree *tree
  *return:	none
  *function:	destroy BiTree
- */
+ *---------------------------------------------------------*/
 void destroyTree(BiTree *tree);
 
-/*name:		insertTreeNodeLeft()
+/*---------------------------------------------------------
+ *name:		insertTreeNodeLeft()
  *input:	BiTree *tree, BiTreeNode *node, const void *data
  *return:	success 0, failure -1
  *function:	insert TreeNode left child
- */
+ *---------------------------------------------------------*/
 int insertTreeNodeLeft(BiTree *tree, BiTreeNode *node, const void *data);
 
-/*name:		insertTreeNodeRight()
+/*---------------------------------------------------------
+ *name:		insertTreeNodeRight()
  *input:	BiTree *tree, BiTreeNode *node, const void *data
  *return:	success 0, failure -1
  *function:	insert TreeNode Right child
- */
+ *---------------------------------------------------------*/
 int insertTreeNodeRight(BiTree *tree, BiTreeNode *node, const void *data);
 
-/*name:		removeTreeNodeLeft()
+/*---------------------------------------------------------
+ *name:		removeTreeNodeLeft()
  *input:	BiTree *tree, BiTreeNode *node
  *return:	none
  *function:	remove TreeNode LeftTree
- */
+ *---------------------------------------------------------*/
 void removeTreeNodeLeft(BiTree *tree, BiTreeNode *node);
 
-/*name:		removeTreeNodeRight()
+/*---------------------------------------------------------
+ *name:		removeTreeNodeRight()
  *input:	BiTree *tree, BiTreeNode *node
  *return:	none
  *function:	remove TreeNode RightTree
- */
+ *---------------------------------------------------------*/
 void removeTreeNodeRight(BiTree *tree, BiTreeNode *node);
 
-/*name:		mergeTree
+/*---------------------------------------------------------
+ *name:		mergeTree
  *input:	BiTree *merge, BiTree *left, BiTree *right, const void *data
  *return:	success 0, failure -1
  *function:	合并left和right所指的树，并将data作为merge根节点数值域
- */
+ *---------------------------------------------------------*/
 int mergeTree(BiTree *merge, BiTree *left, BiTree *right, const void *data);
 
-/*name:		isTreeLeaf()
+/*---------------------------------------------------------
+ *name:		isTreeLeaf()
  *input:	const BiTreeNode *node
  *return:	node is leaf 1, failure 0
  *function:	is node a leaf
- */
+ *---------------------------------------------------------*/
 int isTreeLeaf(const BiTreeNode *node);
 
 #endif
---
+++ tree_binary_search.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include "tree_binary_search.h"
+
+//static void destroy_right(BiSearchTree *biSearchTree, BiTreeNode *node);
+
+
+/*-----------------------------------------------------------------------------
+ *name:		rotateLeft()
+ *argument:	BiTreeNode **node
+ *return:	none
+ *function:	rotate Left
+ *-----------------------------------------------------------------------------*/
+static void rotateLeft(BiTreeNode **node)
+{
+	BiTreeNode *left, *grandchild;
+	left = (*node)->left;
+
+	
+
+}
+
+/*-----------------------------------------------------------------------------
+ *name:
+ *argument:
+ *return:
+ *function:
+ *-----------------------------------------------------------------------------*/
